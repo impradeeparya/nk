690dfa6eb8feefddfb99c083cdd8f6c3d798f20a
Pradeep Arya <pradeep.arya@paytm.com>
2018-05-23 19:47:22 +0530

    nqueen problem

--- src/main/java/backtrack/NQueenSolver.java
+++ src/main/java/backtrack/NQueenSolver.java
@@ -2,85 +2,102 @@ package backtrack;
 
 public class NQueenSolver {
 
-  private static boolean solve(int[][] board) {
+  private static boolean solve(int[][] board, int row, int column) {
 
-    Block block = unassignedBlock(board);
-    if (block == null) {
+    if (row >= board.length) {
       return true;
     }
-    for (int value = 1; value <= 9; value++) {
-      if (isSafe(board, block, value)) {
-        board[block.getX()][block.getY()] = value;
-        if (solve(board)) {
-          return true;
-        }
-        board[block.getX()][block.getY()] = 0;
+    while (column < board[row].length) {
+
+      board[row][column] = 1;
+
+      if (isSafe(board, row, column) && solve(board, row + 1, 0)) {
+        return true;
       }
+      board[row][column] = 0;
+      column++;
     }
 
     return false;
   }
 
-  private static boolean isValidColumn(int[][] board, int row, int value) {
-
-    boolean isValid = true;
-    for (int column = 0; column < board[0].length; column++) {
-      if (board[row][column] == value) {
-
-        isValid = false;
-        break;
-      }
-    }
-    return isValid;
+  private static boolean isSafe(int[][] board, int row, int column) {
+    return isSafeColumn(board, row, column) && isSafeDiagonal(board, row, column);
   }
 
-  private static boolean isValidRow(int[][] board, int column, int value) {
+  private static boolean isSafeDiagonal(int[][] board, int row, int column) {
 
-    boolean isValid = true;
-    for (int rowIndex = 0; rowIndex < board.length; rowIndex++) {
-      if (board[rowIndex][column] == value) {
-        isValid = false;
-        break;
+    int rowIndex = row;
+    int columnIndex = column;
+
+    // up left check
+    rowIndex--;
+    columnIndex--;
+    while (rowIndex >= 0 && columnIndex >= 0) {
+      if (board[rowIndex][columnIndex] == 1) {
+        return false;
       }
+      rowIndex--;
+      columnIndex--;
     }
-    return isValid;
-  }
 
-  private static boolean isValidBoard(int[][] board, int rIndex, int cIndex, int value) {
-
-    for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
-      for (int columnIndex = 0; columnIndex < 3; columnIndex++) {
-        if (board[rowIndex + rIndex][columnIndex + cIndex] == value) {
-          return false;
-        }
+    rowIndex = row;
+    columnIndex = column;
+    // up right check
+    rowIndex--;
+    columnIndex++;
+    while (rowIndex >= 0 && columnIndex < board[row].length) {
+      if (board[rowIndex][columnIndex] == 1) {
+        return false;
       }
+      rowIndex--;
+      columnIndex++;
     }
+
+    rowIndex = row;
+    columnIndex = column;
+    // down left check
+    rowIndex++;
+    columnIndex--;
+    while (rowIndex < board.length && columnIndex >= 0) {
+      if (board[rowIndex][columnIndex] == 1) {
+        return false;
+      }
+      rowIndex++;
+      columnIndex--;
+    }
+
+    rowIndex = row;
+    columnIndex = column;
+    // down right check
+    rowIndex++;
+    columnIndex++;
+    while (rowIndex < board.length && columnIndex < board[row].length) {
+      if (board[rowIndex][columnIndex] == 1) {
+        return false;
+      }
+      rowIndex++;
+      columnIndex++;
+    }
+
     return true;
   }
 
-  private static boolean isSafe(int[][] board, Block block, int value) {
-    int row = block.getX();
-    int column = block.getY();
-    return isValidColumn(board, row, value) && isValidRow(board, column, value)
-        && isValidBoard(board, row - row % 3, column - column % 3, value);
-  }
+  private static boolean isSafeColumn(int[][] board, int row, int column) {
 
-  private static Block unassignedBlock(int[][] board) {
-
-    Block block = null;
-    for (int rowIndex = 0; rowIndex < board.length; rowIndex++) {
-      for (int columnIndex = 0; columnIndex < board[0].length; columnIndex++) {
-
-        if (board[rowIndex][columnIndex] == 0) {
-          block = new Block(rowIndex, columnIndex);
-          break;
-        }
-      }
-      if (block != null) {
-        break;
+    for (int rowIndex = 0; rowIndex < row; rowIndex++) {
+      if (board[rowIndex][column] == 1) {
+        return false;
       }
     }
-    return block;
+
+    for (int rowIndex = row + 1; rowIndex < board.length; rowIndex++) {
+      if (board[rowIndex][column] == 1) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
 
@@ -96,15 +113,16 @@ public class NQueenSolver {
 
   public static void main(String[] args) {
 
-    int[][] board =
-        {{3, 4, 6, 5, 0, 8, 4, 0, 0}, {5, 2, 0, 0, 0, 0, 0, 0, 0}, {0, 8, 7, 0, 0, 0, 0, 3, 1},
-            {0, 0, 3, 0, 1, 0, 0, 8, 0}, {9, 0, 0, 8, 6, 3, 0, 0, 5}, {0, 5, 0, 0, 9, 0, 6, 0, 0},
-            {1, 3, 0, 0, 0, 0, 2, 5, 0}, {0, 0, 0, 0, 0, 0, 0, 7, 4}, {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+    // int[][] board = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
-
+    int[][] board = {{0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
 
     print(board);
-    if (solve(board)) {
+
+    System.out.println("solution : ");
+    if (solve(board, 0, 0)) {
       print(board);
     } else {
       System.out.println("no solution found");
